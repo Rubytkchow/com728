cb904ccd257edee60f0383eba0fd1b6d1d95bb9d
Ruby Chow <rubychow@Rubys-MacBook-Pro.local>
2021-02-05 12:23:13 +0000

    multiline

---
+++ basics/output/multiline_message.py
@@ -0,0 +1,17 @@
+# Display message to the standard output
+print("System Failure Imminent!")
+print("System reboot has been initiated...\n")
+#print() or print("") for new line
+print("...rebooting sensory system")
+print("...rebooting output motors")
+print("...rebooting hover engine\n")
+
+print("System online.")
+
+print("""
+...rebooting sensory system
+...rebooting output motors
+...rebooting hover engine
+
+System online.
+""")
