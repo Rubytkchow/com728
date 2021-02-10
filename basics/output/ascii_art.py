7139bc4e390a9369d1d24791115b64a382f00a4e
Ruby Chow <rubychow@Rubys-MacBook-Pro.local>
2021-02-10 21:02:38 +0000

    eye character

--- basics/output/ascii_art.py
+++ basics/output/ascii_art.py
@@ -3,3 +3,13 @@ print("""
 # O O #
 #######
 """)
+
+# Ask user for eye character
+print("Please enter eye character")
+eye = input()
+
+# Display an ascii art robot
+print("##########")
+print(f"#  {eye}  {eye}   #")
+print("#  ----  #")
+print("##########")
