602b6a043edc58ff60bc388ac22f0d42bd74de2b
Ruby Chow <rubychow@Rubys-MacBook-Pro.local>
2021-02-12 21:49:39 +0000

    Added code to demonstrate the use of a simple for loop.

---
+++ basics/repetitions/for_loop/simple.py
@@ -0,0 +1,14 @@
+print("How many mountains should I display?")
+mountain_num = int(input())
+
+print()
+
+print("Displaying...")
+
+for count in range(mountain_num):
+    print("      __")
+    print("     /  \_")
+    print("    /^    \ ")
+    print("   /  ^    \_")
+    print(" _/ ^ ^     ^\ ")
+    print("/  ^     ^    \ ")
