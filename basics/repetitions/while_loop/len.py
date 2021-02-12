ab961dffc6d5d056e64f7f2c2f773a1fd15b348b
Ruby Chow <rubychow@Rubys-MacBook-Pro.local>
2021-02-12 18:48:37 +0000

    Lucus' version

--- basics/repetitions/while_loop/len.py
+++ basics/repetitions/while_loop/len.py
@@ -7,3 +7,14 @@ print()
 while bop < len(phrase):
     print("bop ", end="")
     bop = bop + 1
+
+userInput = (input("Please enter a phrase:\n"))
+#print(f"{'bop ' * userInput}")
+
+# The while loop way
+
+i = 0
+
+while i < len(userInput):
+    print("bop", end="")
+    i = i + 1
