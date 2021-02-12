bd7caaad55ab59767732c9d097eceb25267875b0
Ruby Chow <rubychow@Rubys-MacBook-Pro.local>
2021-02-12 10:25:05 +0000

    Added code to demonstrate the use of an if...else statement.

---
+++ basics/decisions/simple_decision/if_else.py
@@ -0,0 +1,10 @@
+print("Enter an activity")
+activity = input()
+
+if activity == "calculate":
+    print("Performing Calculations...\n")
+
+else:
+    print("Performing activity...\n")
+
+print("Activity Completed!")
