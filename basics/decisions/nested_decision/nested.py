51ca3c385d9a254a5101aad3ee2186f145806ced
Ruby Chow <rubychow@Rubys-MacBook-Pro.local>
2021-02-12 11:34:06 +0000

    Added code to demonstrate the use of a nested decision statement.

---
+++ basics/decisions/nested_decision/nested.py
@@ -0,0 +1,16 @@
+print("Enter the cover type (hard or soft) of the book?")
+cover_type = input()
+
+if cover_type == "soft":
+    print("Is the book perfect bound?")
+    perfect_bound = input()
+
+    if perfect_bound == "yes":
+      print("Soft cover, perfect bound books are very popular!")
+    else:
+        print("Soft covers with coils or stitches are great for short books")
+
+elif cover_type == "hard":
+    print("Books with hard covers can be more expensive")
+
+
