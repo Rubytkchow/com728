cab9114ff87dc4b25aaa816a9397a2651e6d391d
Ruby Chow <rubychow@Rubys-MacBook-Pro.local>
2021-02-12 17:06:28 +0000

    Added code to demonstrate the use of a simple while loop.

---
+++ basics/repetitions/while_loop/simple.py
@@ -0,0 +1,10 @@
+print("How many cables should I remove?")
+cab_num = int(input())
+
+cab_removed = 0
+
+print()
+
+while cab_removed < cab_num:
+    print("Removed cable")
+    cab_removed = cab_removed + 1
