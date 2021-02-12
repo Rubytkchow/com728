b4d71426f062770992795dd871d50fd9f46602c1
Ruby Chow <rubychow@Rubys-MacBook-Pro.local>
2021-02-12 11:06:01 +0000

    Added code to demonstrate the use of comparison operators.

---
+++ basics/decisions/simple_decision/comparison_operators.py
@@ -0,0 +1,17 @@
+print("Enter the first number")
+first_num = int(input())
+
+print("Enter the Second number")
+second_num = int(input())
+
+if first_num < second_num:
+    print("The first number is the smallest")
+
+elif first_num > second_num:
+    print("The second number is the smallest!")
+
+elif first_num == second_num:
+    print("Both are equal!")
+
+#else:
+    #print("The two numbers are equal."
