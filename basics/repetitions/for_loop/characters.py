8a0a48f53651ed660cd190d2204551d78df1a8f6
Ruby Chow <rubychow@Rubys-MacBook-Pro.local>
2021-02-17 18:24:48 +0000

    Added code to demonstrate the use of for loops in string indexing.

--- basics/repetitions/for_loop/characters.py
+++ basics/repetitions/for_loop/characters.py
@@ -1,1 +1,9 @@
 print("What strange markings do you see?")
+markings = input()
+
+print("Indentifying...")
+
+for count in range(0, len(markings), 1):
+    print(f"index {count}: {markings[count]}")
+
+print("\nDone!")
