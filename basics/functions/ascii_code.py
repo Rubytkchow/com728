4f069ccbd9039a0bf41537391cbd4b4a23877948
Ruby Chow <rubychow@Rubys-MacBook-Pro.local>
2021-02-19 13:45:48 +0000

    Added code to demonstrate the use of built-in functions: len, ord.

---
+++ basics/functions/ascii_code.py
@@ -0,0 +1,8 @@
+print("Program started!")
+print("Please enter a standard character:")
+
+character = input()
+
+print(f"The ASCII code for {character} is {ord(character)}")
+
+print("Program ended!")
