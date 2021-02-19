fe6ee32e05570a74c19d1d7ca626e9e4d7812dd0
Ruby Chow <rubychow@Rubys-MacBook-Pro.local>
2021-02-19 12:05:24 +0000

    Added code to demonstrate the use of one loop nested is another.

---
+++ basics/repetitions/nested_loop/nested.py
@@ -0,0 +1,13 @@
+print("How many rows should I have?")
+rows = int(input())
+
+print("How many columns should I have?")
+columns = int(input())
+
+print("Here I go:\n")
+
+for row in range(0, rows, 1):
+    for column in range(0, columns, 1):
+        print(" :-)", end="")
+    print()
+
