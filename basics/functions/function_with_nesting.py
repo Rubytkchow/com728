89d7aa7bd3b51ad19c645ddbd162d939c179081e
Ruby Chow <rubychow@Rubys-MacBook-Pro.local>
2021-02-25 19:08:51 +0000

    Added code to demonstrate the use of a user-defined function with a nested decision.

---
+++ basics/functions/function_with_nesting.py
@@ -0,0 +1,11 @@
+def identify():
+    print("What do you see?")
+    image = input()
+
+    if image == "A large boulder":
+        print("It's time to run!")
+
+    else:
+        print("We will br fine.")
+
+identify()
