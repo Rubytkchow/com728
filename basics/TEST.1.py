05f890fb7ac56e4b3bd89db587b2399f20f4e57e
Ruby Chow <rubychow@Rubys-MacBook-Pro.local>
2021-02-25 22:50:59 +0000

    formative assessment

---
+++ basics/TEST.1.py
@@ -0,0 +1,35 @@
+# #define function
+# def initiate():
+# #print out statements
+#     print("Initiating Justice League protocol...")
+#     print("Please identify yourself:")
+# #reading user response
+#     identity = input()
+# #printing out message with user response using f-string
+#     print(f"The Justice League protocol has now been initiated for you {identity}.")
+#
+# #function call
+# initiate()
+#
+#
+# #define function using a parameter
+# def unite(num_members):
+#     if num_members >= 7:
+#         print("The Justice League has been united!")
+#     elif num_members < 7:
+#         print("We need more members.")
+#
+# #function calls
+# unite(5)
+# unite(7)
+#
+#
+#
+# #define function
+# def justice(num_members):
+#     for count in range(num_members):
+#         print("Serving Justice...")
+#     print("Justice has been served!")
+#
+# #function call
+# justice(3)
---
+++ htj.py
@@ -0,0 +1,7 @@
+def initiate():
+    print("Initiating Justice League protocol...")
+    print("Please identify yourself:")
+    identity = input()
+    print(f"The Justice League protocol has now been initiated for you {identity}.")
+
+initiate()
