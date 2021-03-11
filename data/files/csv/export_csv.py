bc60ba743dc50ec384ecab732a2f1d3bc58b18fa
Ruby Chow <rubychow@Rubys-MacBook-Pro.local>
2021-03-11 16:35:08 +0000

    Added code to export csv data.

---
+++ data/files/csv/export_csv.py
@@ -0,0 +1,28 @@
+import csv
+
+def export(file_path, integer):
+    print("Exporting...")
+    with open(file_path, "a") as file:
+        print("Please enter the bot id:")
+        bot_id = int(input())
+
+        print("Please enter the bot name:")
+        bot_name = input()
+
+        print("Please enter the bot paint:")
+        bot_paint = input()
+
+        data = f"{bot_id},{bot_name},{bot_paint}\n"
+        file.write(data)
+    print("Done!")
+
+
+def run():
+    export("exported_bots.csv", 2)
+
+
+if __name__ == "__main__":
+    run()
+
+
+
