293f238ed2a38837c2100437bdde0370f1458348
Ruby Chow <rubychow@Rubys-MacBook-Pro.local>
2021-03-12 16:44:48 +0000

    Added code to demonstrate how to populate a list.

---
+++ data/lists/populate_list.py
@@ -0,0 +1,20 @@
+def directions():
+    directions = ["Move Forward", "Move Backward", "Turn Left", "Turn Right"]
+    return directions
+
+def menu():
+    print("Please select a direction:")
+    direct = directions()
+    for index in range(len(direct)):
+        print(f"{index}: {direct}")
+    index = int(input())
+    return direct[index]
+
+def run():
+    route = []
+    print("Working out escape route...")
+    for count in range(0, 5, 1):
+        route.append(menu())
+    print(f"Escape route: {route}")
+
+run()
